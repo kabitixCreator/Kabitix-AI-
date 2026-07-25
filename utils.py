8fb368c194974fad0677901740d15b073ed4f271
kabitixCreator <kabitlego0@gmail.com>
2026-07-25 16:07:54 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,27 @@
+import streamlit as st
 
+def apply_theme():
+    st.markdown("""
+    <style>
+    .stApp{
+        background:#0B0F19;
+    }
+
+    h1,h2,h3{
+        color:white;
+    }
+
+    p,label{
+        color:#C9D1D9;
+    }
+
+    .stButton>button{
+        width:100%;
+        border-radius:12px;
+    }
+
+    .stChatInput{
+        border-radius:20px;
+    }
+    </style>
+    """, unsafe_allow_html=True)
