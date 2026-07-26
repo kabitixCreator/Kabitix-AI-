1b74167c2fcc7b28a40f5022d6e5ccdf272f00ba
kabitixCreator <kabitlego0@gmail.com>
2026-07-26 10:33:10 +0530

    Update logo.py

--- logo.py
+++ logo.py
@@ -2,21 +2,10 @@ import streamlit as st
 
 def show_logo():
     st.markdown("""
-    <div style="text-align:center;padding:5px 0 10px 0;">
-        <h1 style="
-            color:white;
-            font-size:48px;
-            margin-bottom:5px;
-        ">
-            🤖 Kabitix AI
-        </h1>
-
-        <p style="
-            color:#8B949E;
-            font-size:18px;
-            margin-top:0;
-        ">
-            Smart • Fast • Simple
-        </p>
-    </div>
-    """, unsafe_allow_html=True) 
+<div style="text-align:center;padding:10px;">
+<h1 style="font-size:48px;color:white;margin-bottom:5px;">🤖 Kabitix AI</h1>
+<p style="color:#8B949E;font-size:18px;margin-top:0;">
+Smart • Fast • Simple
+</p>
+</div>
+""", unsafe_allow_html=True) 
