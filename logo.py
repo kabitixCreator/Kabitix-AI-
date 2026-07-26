3256c11b970370ff977594918c1fa6d761657e23
kabitixCreator <kabitlego0@gmail.com>
2026-07-26 10:10:54 +0530

    Create logo.py

---
+++ logo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+def show_logo():
+    st.markdown("""
+    <div style="text-align:center;padding:10px;">
+        <h1 style="font-size:45px;margin-bottom:0;">
+            🤖 KTIX AI
+        </h1>
+        <p style="color:#8b949e;font-size:18px;">
+            Smart • Fast • Simple
+        </p>
+    </div>
+    """, unsafe_allow_html=True) 
