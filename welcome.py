3bce5e3ea4886981c483036bb5ea6669d5a49698
kabitixCreator <kabitlego0@gmail.com>
2026-07-26 10:28:15 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -2,23 +2,48 @@ import streamlit as st
 
 def welcome_cards():
     st.markdown("""
-    <div style="display:flex;flex-direction:column;gap:12px;margin-top:15px;">
+    <div style="margin-top:20px;"></div>
 
-        <div style="background:#1E293B;padding:18px;border-radius:15px;">
-            💬 Chat with AI
-        </div>
+    <div style="
+        background:#161B22;
+        padding:15px;
+        border-radius:15px;
+        margin-bottom:10px;
+        color:white;
+        font-size:18px;
+    ">
+    💬 Chat with AI
+    </div>
 
-        <div style="background:#1E293B;padding:18px;border-radius:15px;">
-            📚 AI Study
-        </div>
+    <div style="
+        background:#161B22;
+        padding:15px;
+        border-radius:15px;
+        margin-bottom:10px;
+        color:white;
+        font-size:18px;
+    ">
+    📚 AI Study
+    </div>
 
-        <div style="background:#1E293B;padding:18px;border-radius:15px;">
-            🌍 Translator
-        </div>
+    <div style="
+        background:#161B22;
+        padding:15px;
+        border-radius:15px;
+        margin-bottom:10px;
+        color:white;
+        font-size:18px;
+    ">
+    🌍 Translator
+    </div>
 
-        <div style="background:#1E293B;padding:18px;border-radius:15px;">
-            📖 eBook Creator
-        </div>
-
+    <div style="
+        background:#161B22;
+        padding:15px;
+        border-radius:15px;
+        color:white;
+        font-size:18px;
+    ">
+    📖 eBook Creator
     </div>
     """, unsafe_allow_html=True) 
