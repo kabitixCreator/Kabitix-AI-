672e14385d2e0c1c4f62677a60a95f19b532d6fb
kabitixCreator <kabitlego0@gmail.com>
2026-07-29 16:48:20 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,14 +1,39 @@
 from groq import Groq
 from tavily import TavilyClient
+from memory import load_memory, save_memory 
 import os
 
 # API Clients
 client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 tavily = TavilyClient(api_key=os.getenv("TAVILY_API_KEY"))
 
-def get_ai_response(prompt, pdf_text=""): 
+def get_ai_response(prompt, pdf_text=""):
     try:
+        memory = load_memory()
+
+        if "my name is" in prompt.lower():
+            name = prompt.lower().split("my name is")[-1].strip()
+
+            memory["name"] = name
+            save_memory(memory)
+
+            return f"Nice to meet you, {name}! I will remember your name."
+
+        if "what is my name" in prompt.lower() or "who am i" in prompt.lower():
+
+            if "name" in memory:
+                return f"Your name is {memory['name']}."
+
+            return "I don't know your name yet. Tell me by saying 'My name is ...'"
+
         # Live web search
+        search = tavily.search(
+            query=prompt,
+            search_depth="advanced",
+            max_results=5,
+            include_answer=True
+        ) 
+# Live web search
         search = tavily.search(
     query=prompt,
     search_depth="advanced",
